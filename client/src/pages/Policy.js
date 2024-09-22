836de957d1a57ecee8fc4cb2fc67a9e1395e536a
Nandita10062001 <nanditankr1062001@gmail.com>
2024-09-23 02:14:53 +0530

    Added Policy and About pages

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import Layout from '../components/Layout/Layout'
+import React from 'react';
+import Layout from './../components/Layout/Layout';
 
 const Policy = () => {
   return (
     <Layout>
-        <h1>Privacy Policy</h1>
+      <div className="row contactus">
+        <div className="col-md-6">
+          <img
+            src="/images/policy.jpg"
+            alt="contactus"
+            style={{ maxWidth:'100%', height:'auto'  }}
+          />
+        </div>
+        <div className="col-md-4 policy-content">
+          <h2>Privacy Policy</h2>
+          <div className="policy-box">
+            <p style={{ textAlign: 'justify' }}>
+              At <strong>our company</strong>, we are committed to protecting
+              your privacy and ensuring that your personal information is
+              handled in a safe and responsible manner. The privacy policy
+              outlines how we collect, use, and protect your data when you visit
+              our website or make a purchase from us.
+            </p>
+          </div>
+        </div>
+      </div>
     </Layout>
-  )
-}
+  );
+};
 
 export default Policy;
