0a4c0d9b50b539df1995977d16d49367c3d02955
Nandita10062001 <nanditankr1062001@gmail.com>
2024-09-23 01:33:27 +0530

    React app setup along with styling the nav menu and the 404 page nout found.js

---
+++ client/src/components/Layout/Header.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { RiShoppingBagFill } from "react-icons/ri";
+
+const Header = () => {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <Link className="navbar-brand" to="/">
+            🛒 E-Commerce App
+            </Link>
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink className="nav-link" aria-current="page" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/catgory">
+                  Category
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/register">
+                  Sign Up
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">
+                  Login
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/cart">
+                  Cart (0)
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
